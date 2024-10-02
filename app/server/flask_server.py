4c0fe2cc3d326dca259b12c94b65c836c73755a5
drosset <drosset@tecbrain.com>
2024-10-02 17:32:26 +0200

    [FRONTEND] Se crea conexion entre frontend y backend

--- app/server/flask_server.py
+++ app/server/flask_server.py
@@ -5,16 +5,24 @@ import sys
 project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(project_root)
 
-from flask import Flask
-from llm.generate_recipes import GenerateRecipes
 
+from llm.generate_recipes import GenerateRecipes
+from flask_cors import CORS
 from flask import Flask
 from neo4j import GraphDatabase
 import neo4j
 
 
-app = Flask(__name__)
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello-World!</p>"
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {"message": "Hello from Flask!"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
